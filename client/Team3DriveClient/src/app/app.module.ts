081387fb9fa50bf751d28367f08a07115e683519
שירן הרוני <shiran@ramadered.local>
2020-04-19 12:37:46 +0300

    search not finish but working

--- client/Team3DriveClient/src/app/app.module.ts
+++ client/Team3DriveClient/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FilesComponent } from './files/files.component';
@@ -16,7 +16,8 @@ import { SearchComponent } from './search/search.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/Team3DriveClient/src/app/files/files.component.ts
+++ client/Team3DriveClient/src/app/files/files.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { File } from '../file';
 import { FileService } from '../file.service';
-
 @Component({
   selector: 'app-files',
   templateUrl: './files.component.html',
@@ -9,13 +8,29 @@ import { FileService } from '../file.service';
 })
 export class FilesComponent implements OnInit {
   files: File[];
+  filteredFiles: File[];
+  private _searchTerm: string;
+
   constructor(private fileService: FileService) { }
 
   ngOnInit(){
     this.getFiles();
+    this.filteredFiles = this.files;
   }
+
   getFiles(): void {
     this.fileService.getFiles()
     .subscribe(files => this.files = files);
+  }
+
+  get searchTerm():string{
+    return this._searchTerm;
+  }
+  set searchTerm(value: string){
+    this._searchTerm = value;
+    this.filteredFiles = this.filterFiles(value);
+  }
+  filterFiles(searchString: string){
+    return this.files.filter(file=>file.name.toLocaleLowerCase().indexOf(searchString.toLocaleLowerCase()) !== -1);
   }
 }
